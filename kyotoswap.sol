fb68c0fb3f07f4b9c110221f513067241ead016c
Abdul Rahman bin Ibrahim <rahman@xoxzo.com>
2021-10-30 00:46:01 +0800

    moved indent further left

--- kyotoswap.sol
+++ kyotoswap.sol
@@ -5,43 +5,43 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/utils/math/SafeMath.sol";
 
 contract KyotoSwap is IERC20, Ownable {
-	using SafeMath for uint;
+    using SafeMath for uint;
 
-	IERC20 public busd;
-	IERC20 public kyo;
-	uint public amountbusd;
-	uint public amountkyo;
-	uint public availablekyo;
+    IERC20 public busd;
+    IERC20 public kyo;
+    uint public amountbusd;
+    uint public amountkyo;
+    uint public availablekyo;
 
-	constructor(
-	    address _busd,
-            address _kyo,
-            uint _availablekyo;
-	){
-	    busd = IERC20(_busd);
-	    kyo = IERC20(_kyo);
-	    kyo.increaseAllowance(address(this), _availablekyo);
-	    availablekyo = _availablekyo;
-	}
+    constructor(
+	address _busd,
+        address _kyo,
+        uint _availablekyo;
+    ){
+	busd = IERC20(_busd);
+	kyo = IERC20(_kyo);
+	kyo.increaseAllowance(address(this), _availablekyo);
+	availablekyo = _availablekyo;
+    }
 
-	function swap(uint _amountbusd, uint _amountkyo) public {
-	    require(_amountbusd > 0);
-	    require(_amountbusd <= busd.balanceOf[_msgSender()]);
-	    _amountkyo = _amountbusd.tryDiv(10);
-	    require(_amountkyo >= availablekyo);
+    function swap(uint _amountbusd, uint _amountkyo) public {
+	require(_amountbusd > 0);
+	require(_amountbusd <= busd.balanceOf[_msgSender()]);
+	_amountkyo = _amountbusd.tryDiv(10);
+	require(_amountkyo >= availablekyo);
 		
-	    busd.increaseAllowance(address(this), _amountbusd);
-	    busd.transferFrom(_msgSender(), owner(), _amountbusd);
+	busd.increaseAllowance(address(this), _amountbusd);
+	busd.transferFrom(_msgSender(), owner(), _amountbusd);
 	    
-	    kyo.transferFrom(owner(), _msgSender(), _amountkyo);
-	    availablekyo = availablekyo.trySub(_amountkyo);		
-	}
+	kyo.transferFrom(owner(), _msgSender(), _amountkyo);
+	availablekyo = availablekyo.trySub(_amountkyo);		
+    }
 
-	function addkyo(uint _amountkyo) public onlyOwner {
-	    require(_amountkyo > 0);
-	    require(_amountkyo <= kyo.balanceOf[_msgSender()]);
+    function addkyo(uint _amountkyo) public onlyOwner {
+	require(_amountkyo > 0);
+	require(_amountkyo <= kyo.balanceOf[_msgSender()]);
 	    
-	    kyo.increaseAllowance(address(this), _amountkyo);
-	    availablekyo = availablekyo.tryAdd(_amountkyo);
-	}
+	kyo.increaseAllowance(address(this), _amountkyo);
+	availablekyo = availablekyo.tryAdd(_amountkyo);
+    }
 }
