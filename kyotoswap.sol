81bfe854bff66a4852d45adfd1e456b2085120f2
Abdul Rahman bin Ibrahim <rahman@xoxzo.com>
2021-10-31 11:47:45 +0800

    working with implementations

--- kyotoswap.sol
+++ kyotoswap.sol
@@ -5,7 +5,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/utils/math/SafeMath.sol";
 
-abstract contract KyotoSwap is IERC20, Ownable {
+contract KyotoSwap is IERC20, Ownable {
 	using SafeMath for uint256;
 
 	IERC20 public busd;
@@ -40,4 +40,12 @@ abstract contract KyotoSwap is IERC20, Ownable {
 		kyo.approve(address(this), _amountkyo);
 		amountkyo = amountkyo.add(_amountkyo);
 	}
+	
+	// implementations required to use interface
+    function allowance(address owner, address spender) external override view returns (uint256) {}
+    function approve(address spender, uint256 amount) external override returns (bool) {}
+    function balanceOf(address account) external override view returns (uint256) {}
+    function totalSupply() external override view returns (uint256) {}
+    function transfer(address recipient, uint256 amount) external override returns (bool) {}
+    function transferFrom(address sender,  address recipient, uint256 amount) external override returns (bool) {}
 }
