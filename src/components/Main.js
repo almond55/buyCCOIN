9cba46f8a31cd6f6a2239e132159349a35a8480b
Betawi1 <Rashdan.ibrahim@gmail.com>
2021-11-02 23:35:24 +0800

    Update Main.js
    ask to check network

--- src/components/Main.js
+++ src/components/Main.js
@@ -36,7 +36,7 @@ class Main extends Component {
         <div className="card mb-4" >
           <div className="card-body">
             *PLEASE READ BEFORE DOING THE SWAP* <br/><br/>
-            This KyotoSwap App is a decentralized application or DApp. <br/><br/> Desktop and Laptop user can connect to this DApp using your Metamask wallet. Mobile user (non IOS) can connect using your Trust Wallet or Metamask mobile app.<br/><br/>Please make sure that the public address shown beside the identicon matches your wallet address before you make the swap<br/><br/>User can only swap BUSD (Binance-Peg BUSD Token, token address: 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56) for Kyotocoin (KYO, token address: 0xaEBC9EbDd1CD6808b632aA675eA571EEddD0C5b4).<br/><br/>User who does not have BUSD can purchase it by using your credit/debit card on Moonpay.com, please make sure to select 'BUSD - Binance USD (BEP-20)'.
+            This KyotoSwap App is a decentralized application or DApp. <br/><br/> Desktop and Laptop user can connect to this DApp using your Metamask wallet. Mobile user (non IOS) can connect using your Trust Wallet or Metamask mobile app.<br/><br/>Please make sure that you change your wallet network to 'Smart Chain' and the public address shown beside the identicon matches your wallet address before you make the swap<br/><br/>User can only swap BUSD (Binance-Peg BUSD Token, token address: 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56) for Kyotocoin (KYO, token address: 0xaEBC9EbDd1CD6808b632aA675eA571EEddD0C5b4).<br/><br/>User who does not have BUSD can purchase it by using your credit/debit card on Moonpay.com, please make sure to select 'BUSD - Binance USD (BEP-20)'.
           </div>
         </div>
       </div>
