06c184a24101feaa9e423300baed1f3ec76cc57f
rahman.ibrahim <rahman.ibrahim@hypaship.com>
2022-04-29 22:37:06 +0800

    Complete new build

---
+++ build/precache-manifest.88f9f24dba7e0bacc7a0e7c8485c7f2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdb79095ca875cbb0b94",
+    "url": "/static/css/main.c6e39439.chunk.css"
+  },
+  {
+    "revision": "cdb79095ca875cbb0b94",
+    "url": "/static/js/main.cdb79095.chunk.js"
+  },
+  {
+    "revision": "4154d2530261299bdd5c",
+    "url": "/static/css/1.baa26ce8.chunk.css"
+  },
+  {
+    "revision": "4154d2530261299bdd5c",
+    "url": "/static/js/1.4154d253.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "17a1d2c5a6c3e427903afb98e80b350a",
+    "url": "/static/media/carbondex.17a1d2c5.png"
+  },
+  {
+    "revision": "73b80ffa43127d4bdf3480d583a61d43",
+    "url": "/static/media/CCOIN.73b80ffa.png"
+  },
+  {
+    "revision": "55cf64af05b0578e0b9801bf95328a6b",
+    "url": "/index.html"
+  }
+];
